5643bafda75f7bb253cc811630ad31e790ae488a
Zach Dwiel <zdwiel@gmail.com>
2019-01-08 10:46:21 -0500

    update sync

--- sync_all.py
+++ sync_all.py
@@ -1,13 +1,29 @@
 #!/usr/local/bin/python3
 import os
+from multiprocessing import Process
+
+
+def system_process(command):
+    os.system(command)
 
 
 def sync(project_name, server_name=None, remote_home=None):
-    os.system(f"./sync_rsync {project_name} {server_name} {remote_home}")
+    p = Process(
+        target=system_process,
+        args=(f"./sync_rsync {project_name} {server_name} {remote_home}",),
+    )
+    p.start()
+    return p
 
 
 def main():
-    sync("coach", "nervana-titanxp10.fm.intel.com", "/nfs/site/home/zdwiel")
+    processes = [
+        sync("coach", "nervana-titanxp10.fm.intel.com", "/nfs/site/home/zdwiel"),
+        sync("bottleneck", "nervana-titanxp10.fm.intel.com", "/nfs/site/home/zdwiel"),
+    ]
+
+    for process in processes:
+        process.join()
 
 
 if __name__ == "__main__":
